33950e058e17a22f66d274e8c331cb5acb075620
Micha Strobl <m.strobl@campus.tu-berlin.de>
2023-05-16 15:21:46 +0200

    Rapha's corrections for plot function

--- src/utils.py
+++ src/utils.py
@@ -35,14 +35,14 @@ class absorption_coefficient:
         self.d = d
 
     def plot(self):
-        fig, ax = px.line()
+        fig = go.Figure()
         x = np.linspace(self.f_start, self.f_end, 10)
         alpha = 0.9 * np.log10(x*self.d) - 2.4
         alpha[alpha > 1] = 1
         alpha[alpha < 0] = 0
-        ax.plot(x, alpha)
-        ax.set_xlim(self.f_start, self.f_end)
-        ax.set_xlabel('Frequency in [Hz]')
-        ax.set_ylabel('Absorption Coefficient')
-        ax.set_title('Absorption coefficient of a {} mm material'.format(self.d))
+        fig.add_trace(go.Scatter(x=x, y=alpha, mode='lines'))
+        fig.update_layout(yaxis_range=[0, 1.1])
+        fig.update_layout(title='Absorption coefficient of a {} mm material'.format(self.d),
+                          xaxis_title='Frequency in [Hz]',
+                          yaxis_title='Absorption Coefficient')
         return fig
