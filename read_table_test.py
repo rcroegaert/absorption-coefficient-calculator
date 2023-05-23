4af39ea5cb49fdba080ef74fda80b9d661e677d3
Michael Strobl <m.strobl@campus.tu-berlin.de>
2023-05-23 15:02:01 +0200

    improved reading from data table

--- read_table_test.py
+++ read_table_test.py
@@ -1,14 +1,31 @@
 import pandas as pd
 
-material_data = pd.read_csv('data.csv', sep=';')
+class DataTableReader:
+    
+    def __init__(self):
+        self.material_data = pd.read_csv('data.csv', sep=';', index_col='Material')
 
-# material_data.drop(0)
-# material_data.to_csv('data.csv', sep=';')
+    def get_c(self, material):
+        try:
+            row_data = self.material_data.loc[material]['c']
+            return row_data
+        except KeyError:
+            print(f'Material {material} not found in table!')
+            return None
+
+    def print_table(self):
+        print(self.material_data.to_markdown())
+
+    
+data_table = DataTableReader()
 
 # return speed of sound for Blei
-c_lead = material_data['Material', 'c']
-c_lead.get('Blei')
+print('Speed of sound in lead: ' + str(data_table.get_c('Blei')))
+# see what happens when material does not exist in table
+print('Speed of sound in a muffin: ' + str(data_table.get_c('Muffin')))
 
-# check if a material exists in table
 
-print(material_data.to_markdown())
+# Luft exists multiple times in table
+# To-Do: case handling for multiple entries (go by temperature)
+
+data_table.print_table()
